b9e4e3261c49dc2a32d808e1844cba0c22b9f8a0
Sherlemious <abd.moh.yousef@gmail.com>
2020-05-30 23:39:09 +0200

    The error line output will be incremented so that the first line is output as Line number #1 instead of number #0. This makes it easier for the user to identify the line number where the error has occurred. Added Errors:  1- Tocabsent: This is print when a value to be compared is neither a number or a variable.

--- Errors.py
+++ Errors.py
@@ -7,7 +7,7 @@ class Error:
 
     def isprint(self):
         print(self.printed)
-        print(f"This error has been generated in Line number #{no}")
+        print(f"This error has been generated in Line number #{no+1}")
 
 
 VarNotPresent = Error("Variable Identifier does not exist.")
@@ -16,3 +16,5 @@ NoEqualSpaces = Error(
     "Please make sure that there is a space " " character between each identifier(variable) operand, or an equal sign")
 
 OpInvalid = Error("There is an invalid operand")
+
+Tocabsent = Error("There is an invalid operand. Please input a valid variable or number")
