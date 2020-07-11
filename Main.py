d5da5c27a61a01d893e1aa052a07c625cfd74436
Sherlemious <abd.moh.yousef@gmail.com>
2020-07-11 15:04:35 +0200

    The Main.py now contains only the function names and imports only the Command file and the Config file.

--- Main.py
+++ Main.py
@@ -1,217 +1,37 @@
-from Functions import *
-
+import Commands as cc
 # Variables
-variables = {}
-Flags = {}
-LineList = []
-counter = 0
-tobeeval = ""
-i = 0
-
-
-# Functions
-def IF():
-    global Lst2, toc1, toc2, op1, Flags
-    Lst2 = Line.split()
-    # First Variable or Number
-    if Lst2[1] in variables:
-        toc1 = variables[Lst2[1]]
-    elif type(float(Lst2[1])) == float or type(int(Lst2[1])) == int:
-        toc1 = variables[Lst2[1]]
-    else:
-        Tocabsent.isprint()
-    # Second Variable or Number
-    if Lst2[3] in variables:
-        toc2 = variables[Lst2[3]]
-    elif type(Lst2[3]) == float or type(Lst2[3]) == int:
-        toc2 = variables[Lst2[3]]
-    # Comparison type
-    if Lst2[2] in op_list:
-        Flags[counter] = op_dict(toc1, toc2)[Lst2[2]]
-    else:
-        OpInvalid.isprint()
-
+import Config
 
 # Opening the file
 File = open("F:\\Projects\\Applications\\IGCSE-CS-PC-Compiler\\To be translated", "r")
-FileList = list(File)
+Config.FileList = list(File)
 File.close()
 # Manipulating File Done
 
-while i < len(FileList):  # Iterates through the lines until no more lines are available
-    Line = FileList[i]
-    Line = Line.rstrip('\n')
+while Config.i < len(Config.FileList):  # Iterates through the lines until no more lines are available
+    Config.Line = Config.FileList[Config.i]
+    Config.Line = Config.Line.rstrip('\n')
 
-    if Line[0:5] == "PRINT":
-        lst1 = Line.split()
-        printed = ""
-        for w in range(1, len(lst1)):
+    if Config.Line[0:5] == "PRINT":
+        cc.PRINT()
 
-            word = lst1[w]
-            end = int(len(word))
-            if word in variables and word[0] != "\"":  # This checks if it is a variable and if the variable exists
-                printed += str(variables[word])
-            else:
-                for c in range(end):
-                    if w == 1 and c == 0:
-                        pass
-                    else:
-                        printed += word[c]
-                printed += " "
+    elif Config.Line[0:5] == "WHILE":
+        cc.WHILE()
 
-        print(printed)
+    elif Config.Line[0:6] == "REPEAT":
+        cc.REPEAT()
 
-    elif Line[0:5] == "WHILE":
-        # Defining the condition
-        IF()
+    elif Config.Line[0:5] == "INPUT":
+        cc.INPUT()
 
-        Line = FileList[i]
-        Lst = Line.split()
-        LineList.append(Line)
-        end2 += 1
+    elif Config.Line[0:3] == "FOR":
+        cc.FOR()
 
-        while Lst[0] != "ENDWHILE":
-            st3 = counter
-            end3 = counter
-            i += 1
-            Line = FileList[i]
-            Lst = Line.split()
-            LineList.append(Line)
-            end3 += 1
-
-        while Flags[counter]:
-            for lf in LineList:
-
-                if lf[0:5] == "PRINT":
-                    lst1 = lf.split()
-                    printed = ""
-                    for w in range(1, len(lst1)):
-
-                        word = lst1[w]
-
-                        end = int(len(word))
-                        if word in variables and word[
-                            0] != "\"":  # This checks if it is a variable and if the variable exists
-                            printed += variables[word]
-                        else:
-                            for c in range(end):
-                                if w == 1 and c == 0 or w == len(lst1) - 1 and c == end - 1:
-                                    pass
-                                else:
-                                    printed += word[c]
-                            printed += " "
-                        print(printed)
-
-                elif lf[0:5] == "WHILE":
-                    WHILE(File, Flags, no, variables, Line)
-
-                elif lf[0:6] == "REPEAT":
-                    REPEAT()  # Haven't added repeat
-
-                elif lf[0:5] == "INPUT":
-                    variables[Line[6:len(Line) - 1]] = input()
-
-                elif lf[0:3] == "FOR":
-                    FOR(Line, File)
-
-                elif lf[0:2] == "IF":
-                    IF(Line, variables, Flags)
-            IF()
-
-    elif Line[0:6] == "REPEAT":
-        pass
-
-    elif Line[0:5] == "INPUT":
-        varwanted = Line.split()
-        varwanted = varwanted[1]
-        variables[varwanted] = input()
-
-    elif Line[0:3] == "FOR":
-        Lst = Line.split()
-        LCV = Lst[1]
-        Start = int(Lst[3])
-        End = int(Lst[5])
-        variables[LCV] = LCV
-        while Lst[0] != "ENDFOR":
-            st2 = counter
-            end2 = counter
-            i += 1
-            Line = FileList[i]
-            Lst = Line.split()
-            if Lst[0] == "ENDFOR":
-                break
-            LineList.append(Line)
-            end2 += 1
-        Line = FileList[i]
-        for con in range(Start, End + 1):
-            variables[LCV] = con
-            for lf in LineList:
-                if lf[0:5] == "PRINT":
-                    lst1 = lf.split()
-                    printed = ""
-                    for w in range(1, len(lst1)):
-
-                        word = lst1[w]
-
-                        end = int(len(word))
-                        if word in variables and word[
-                            0] != "\"":  # This checks if it is a variable and if the variable exists
-                            printed += variables[word]
-                        else:
-                            for c in range(end):
-                                if w == 1 and c == 0 or w == len(lst1) - 1 and c == end - 1:
-                                    pass
-                                else:
-                                    printed += word[c]
-                            printed += " "
-                        print(printed)
-
-                elif lf[0:5] == "WHILE":
-                    WHILE(File, Flags, no, variables, Line)
-
-                elif lf[0:6] == "REPEAT":
-                    REPEAT()  # Haven't added repeat
-
-                elif lf[0:5] == "INPUT":
-                    variables[Line[6:len(Line) - 1]] = input()
-
-                elif lf[0:3] == "FOR":
-                    FOR(Line, File)
-
-                elif lf[0:2] == "IF":
-                    IF(Line, variables, Flags)
-
-                # Assignment statement
-                else:
-                    eqfound = False
-                    tobeeval = ""
-                    lst3 = lf.split()
-                    for vr in lst3:
-                        if vr == "=" or eqfound:
-                            eqfound = True
-                        if eqfound:
-                            if vr in variables:
-                                tobeeval += str(variables[vr])
-                            elif isinstance(vr, int):
-                                tobeeval += vr
-                    variables[lst3[0]] = eval(tobeeval[:])
-                    variables[LCV] = con
-    elif Line[0:2] == "IF":
-        IF()
+    elif Config.Line[0:2] == "IF":
+        cc.IF()
 
     else:
-        tobeeval = ""
-        eqfound = False
-        lst3 = Line.split()
-        for vr in lst3:
-            if vr == "=" or eqfound:
-                eqfound = True
-            if eqfound:
-                if vr in variables:
-                    tobeeval += str(variables[vr])
-                elif isinstance(vr, int):
-                    tobeeval += vr
-        variables[lst3[0]] = eval(tobeeval[1:])
+        cc.ASSIGNMENT(Config.Line)
 
-    counter += 1
-    i += 1
+    Config.counter += 1
+    Config.i += 1
