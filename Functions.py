8c2b66537699d0098ac1c85a64c8280b15c8d093
Sherlemious <abd.moh.yousef@gmail.com>
2020-07-30 18:23:04 +0200

    Added the compare function
    This function will be used to carry out comparisons which are necessary for the IF statement, as well as the WHILE and REPEAT loops.

--- Functions.py
+++ Functions.py
@@ -1,4 +1,5 @@
 import Config
+import Errors
 
 
 def op_dict(toc1, toc2):
@@ -17,3 +18,25 @@ def LineCounter(filename):
     for line in filename:
         counter += 1
     return counter
+
+
+def comp(val1, val2, operand):
+    Lst2 = Config.Line.split()
+    # First Variable or Number
+    if val1 in Config.variables:
+        toc1 = Config.variables[val1]
+    elif type(float(val1)) == float or type(int(val1)) == int:
+        toc1 = float(val1)
+    else:
+        Errors.Tocabsent.isprint()
+    # Second Variable or Number
+    if val2 in Config.variables:
+        toc2 = Config.variables[val2]
+    elif type(float(val2)) == float or type(int(val2)) == int:
+        toc2 = float(val2)
+    # Comparison type
+    if operand in Config.op_list:
+        Config.Flags[Config.counter] = op_dict(toc1, toc2)[operand]
+        return op_dict(toc1, toc2)[operand]
+    else:
+        Errors.OpInvalid.isprint()
