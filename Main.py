0fe20f2066c86f324da3cbd99e0102988578dee3
Sherlemious <abd.moh.yousef@gmail.com>
2020-09-09 11:40:04 +0200

    Added the Removeend function and added a continue line after each elif.

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
 import Commands as Do
 import Config
+import Functions as Fun
 
 # Opening the file
 File = open("F:\\Projects\\Applications\\IGCSE-CS-PC-Transpiler\\To be translated", "r")
 Config.FileList = list(File)
 File.close()
 # Manipulating File Done
+Fun.Removeend(Config.FileList)
 
 while Config.i < len(Config.FileList):  # Iterates through the lines until no more lines are available
     Config.Line = Config.FileList[Config.i]
@@ -13,21 +15,27 @@ while Config.i < len(Config.FileList):  # Iterates through the lines until no mo
 
     if Config.Line[0:5] == "PRINT":
         Do.PRINT(Config.Line)
+        continue
 
     elif Config.Line[0:5] == "WHILE":
         Do.WHILE(Config.Line)
+        continue
 
     elif Config.Line[0:6] == "REPEAT":
         Do.REPEAT()
+        continue
 
     elif Config.Line[0:5] == "INPUT":
         Do.INPUT(Config.Line)
+        continue
 
     elif Config.Line[0:3] == "FOR":
         Do.FOR()
+        continue
 
     elif Config.Line[0:2] == "IF":
         Do.IF(Config.Line)
+        continue
 
     else:
         Do.ASSIGNMENT(Config.Line)
