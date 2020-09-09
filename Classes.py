bb464f0eb0574b9dc28741e1e7cbc128fa3c3f28
Sherlemious <abd.moh.yousef@gmail.com>
2020-09-09 11:53:44 +0200

    Create Classes.py

---
+++ Classes.py
@@ -0,0 +1,7 @@
+class Loop:
+    def __init__(self, linelist, LCV, start, end):
+        self.linelist = linelist
+        self.LCV = LCV
+        self.start = start
+        self.end = end
+        self.curlinenumber = 0
