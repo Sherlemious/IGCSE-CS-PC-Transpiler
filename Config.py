fc46c99e8cafc14ce104234a52e81a27100c837f
Sherlemious <abd.moh.yousef@gmail.com>
2020-09-15 20:55:06 +0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,7 +5,7 @@ counter = 0
 tobeeval = ""
 i = 0
 iteratables = []
-CountDict = {"IF": 0, "WHILE": 0, "REPEAT": 0}
+CountDict = {"IFSTATEMENT": 0, "WHILELOOP": 0, "REPEATLOOP": 0}
 FileList = []
 op_list = ['=', '>', '<', '<=', '>=', '<>']
 mops = ["+", "-", "*", "/"]
