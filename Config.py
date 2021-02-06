5e61642c4e0d673a85142452ad73c61d8cab8d93
Abdulrahman <abd.moh.yousef@gmail.com>
2021-02-06 20:47:41 +0200

    Removed no longer necessary variables.

--- Config.py
+++ Config.py
@@ -3,9 +3,7 @@ variables = {}
 Flags = {}
 counter = 0
 to_be_eval = ""
-i = 0
 iteratables = []
-CountDict = {"IF_ST": 0, "WHILE_L": 0, "REPEAT_L": 0}
 FileList = []
 op_list = ['=', '>', '<', '<=', '>=', '<>']
 logic_list = ['AND', "OR", "XOR", "NAND", "NOR"]
