126383031293d2dc5457ec6c1e43b044fa4e7e39
Abdulrahman <abd.moh.yousef@gmail.com>
2021-02-07 19:27:14 +0200

    Fixed couple bugs (No specific changes)

--- Commands.py
+++ Commands.py
@@ -4,8 +4,6 @@ import Classes
 
 
 def main(line_used):
-    Fun.check_opener_ending(line_used)
-
     if line_used[0:5] == "PRINT":
         PRINT(line_used)
 
@@ -31,9 +29,9 @@ def main(line_used):
 def IF():
     Fun.object_gen()
     Config.iteratables[-1] = Classes.IF_STATEMENT()
-    Classes.Loop_Counts.If += 1
 
-    outer_list = []
+    true_if_added = 0
+    false_if_added = 0
     true_lines = []
     false_lines = []
 
@@ -42,44 +40,64 @@ def IF():
     if line_split[-1] == "THEN":
         del line_split[-1]
 
-    while line_split != "ENDIF":
-        Config.iteratables[-2].line_number += 2
+    Config.iteratables[-2].line_number += 1
+    while line_split[0] != "ENDIF":
+        Config.iteratables[-2].line_number += 1
         try:
-            Config.iteratables[-2].cur_line = Config.FileList[Config.iteratables[-2].line_number]
-            outer_list.append(Config.iteratables[-2].cur_line)
+            Config.iteratables[-2].cur_line = Config.iteratables[-2].line_list[Config.iteratables[-2].line_number]
 
             true_lines.append(Config.iteratables[-2].cur_line)
             line_split = Config.iteratables[-2].cur_line.split()
+
+            Fun.check_opener_ending(Config.iteratables[-2].cur_line, "IF")
+            if line_split[0] == "IF":
+                true_if_added += 1
+            if line_split[0] == "ENDIF":
+                true_if_added -= 1
+
             if line_split[0] == "ELSE" and Classes.Loop_Counts.If == 1:
+                del true_lines[-1]
                 while True:
                     Config.iteratables[-2].line_number += 1
                     try:
+
                         Config.iteratables[-2].cur_line = Config.iteratables[-2].line_list[
                             Config.iteratables[-2].line_number]
-                        false_lines.append(Config.Line)
+                        false_lines.append(Config.iteratables[-2].cur_line)
                         line_split = Config.iteratables[-2].cur_line.split()
+
+                        Fun.check_opener_ending(Config.iteratables[-2].cur_line, "IF")
+                        if line_split[0] == "IF":
+                            false_if_added += 1
+                        if line_split[0] == "ENDIF":
+                            false_if_added -= 1
+
                     except IndexError:
                         pass
-                    if line_split[0] != "ELSE" and line_split[0] != "ENDIF" or Classes.Loop_Counts.If != 1:
-                        continue
-                    del false_lines[-1]
-                    Classes.Loop_Counts.If -= 1
+                    if line_split[0] == "ENDIF" and Classes.Loop_Counts.If == 1:
+                        del false_lines[-1]
+                        break
+            else:
+                if line_split[0] == "ENDIF" and Classes.Loop_Counts.If == 1:
+                    del true_lines[-1]
                     break
 
         except IndexError:
             pass
 
-        cond = Config.iteratables[-1].condition
-        if len(cond) == 4:
-            Config.iteratables[-1].condition = Fun.comp(cond[1], cond[3], cond[2])
-            if Config.iteratables[-1].condition:
-                Config.iteratables[-1].line_list = Config.iteratables[-1].If_True
-            else:
-                Config.iteratables[-1].line_list = Config.iteratables[-1].If_False
-            while Config.iteratables[-1].line_number < len(Config.iteratables[-1].If_True):
-                cur_line = Config.iteratables[-1].line_list[Config.iteratables[-1].line_number]
-                main(cur_line)
-                Config.iteratables[-1].line_number += 1
+    cond = Config.iteratables[-1].condition
+    if len(cond) == 4:
+        Config.iteratables[-1].condition = Fun.comp(cond[1], cond[3], cond[2])
+        if Config.iteratables[-1].condition:
+            Config.iteratables[-1].line_list = true_lines
+            Classes.Loop_Counts.If -= false_if_added
+        else:
+            Config.iteratables[-1].line_list = false_lines
+            Classes.Loop_Counts.If -= true_if_added
+        while Config.iteratables[-1].line_number < len(Config.iteratables[-1].line_list):
+            cur_line = Config.iteratables[-1].line_list[Config.iteratables[-1].line_number]
+            main(cur_line)
+            Config.iteratables[-1].line_number += 1
 
 
 def FOR():
