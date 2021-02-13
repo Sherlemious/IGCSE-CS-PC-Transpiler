a5cdc53573ab3c61d543b947245b4285dac46db9
Abdulrahman <abd.moh.yousef@gmail.com>
2021-02-13 20:53:20 +0200

    Added comments and reverted a previous edit

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import Config
 import Functions as Fun
 
 
-# Taking in the code (currently through a text file)
+# Taking in the Pseudocode (currently through a text file)
 dirname = os.path.dirname(__file__)
 dirname += "\\To be translated"
 File = open(dirname, "r")
@@ -15,9 +15,10 @@ File.close()
 Fun.rem_end(Config.FileList)
 
 Fun.object_gen()
-Config.iteratables[0] = Classes.FUNCTION(Config.FileList)
+Config.Iteratables[0] = Classes.FUNCTION(Config.FileList)
 
-while Config.iteratables[-1].line_number <= len(Config.iteratables[-1].line_list):
-    cur_line = Config.iteratables[-1].line_list[Config.iteratables[-1].line_number]
+# Iterates through the list of the lines
+while Config.Iteratables[-1].line_number < len(Config.Iteratables[-1].line_list):
+    cur_line = Config.Iteratables[-1].line_list[Config.Iteratables[-1].line_number]
     Do.main(cur_line)
-    Config.iteratables[-1].line_number += 1
+    Config.Iteratables[-1].line_number += 1
