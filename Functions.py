5b74e1bcca537ad179b22d553911ada58820f5de
Abdulrahman <abd.moh.yousef@gmail.com>
2021-02-24 17:23:31 +0200

    Simplified the 2 operands comparison function.

--- Functions.py
+++ Functions.py
@@ -25,37 +25,36 @@ def log_dict(exp1, exp2):
     }
 
 
+def find_value(val):
+    if val in Config.variables:
+        try:
+            toc = float(Config.variables[val])
+        except ValueError:
+            toc = Config.variables[val]
+    elif "[" in val:
+        toc = fetch_value(val)
+    elif "\"" in val:
+        toc = val[1:-1]
+    else:
+        try:
+            toc = float(val)
+            toc = float(val)
+        except ValueError:
+            pass
+    return toc
+
+
 def comp(val1, val2, operand):
     # First Variable or Number
-    flag1 = False
-    flag2 = False
-    if val1 in Config.variables:
-        toc1 = float(Config.variables[val1])
-        flag1 = True
-    elif type(float(val1)) == float or type(int(val1)) == int:
-        toc1 = float(val1)
-        flag1 = True
-    elif "[" in val1:
-        fetch_value(val1)
-    else:
-        Errors.Absent.isprint()
+    toc1 = find_value(val1)
+
     # Second Variable or Number
-    if flag1:
-        if val2 in Config.variables:
-            toc2 = float(Config.variables[val2])
-            flag2 = True
-        elif type(float(val2)) == float or type(int(val2)) == int:
-            toc2 = float(val2)
-            flag2 = True
-        elif "[" in val2:
-            fetch_value(val2)
-        # Comparison type
-        if flag1 and flag2:
-            if operand in Config.op_list:
-                Config.Flags[Config.counter] = op_dict(toc1, toc2)[operand]
-                return op_dict(toc1, toc2)[operand]
-            else:
-                Errors.OpInvalid.isprint()
+    toc2 = find_value(val2)
+    # Comparison
+    if operand in Config.op_list:
+        return op_dict(toc1, toc2)[operand]
+    else:
+        Errors.OpInvalid.isprint()
 
 
 # Compares two expressions by using comp function
